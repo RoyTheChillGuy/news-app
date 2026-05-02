4bed1df3a6c6751dda721e8da56d2836f5832b05
RoyTheChillGuy <aroya.ppa@gmail.com>
2026-05-02 13:23:20 +0700

    Delete skl_news directory

--- skl_news/android/app/src/main/kotlin/com/example/skl_news/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.skl_news
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- skl_news/lib/bindings/home_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:skl_news/controllers/weather_controller.dart';
-
-class HomeBinding implements Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut<WeatherController>(() => WeatherController());
-  }
-}
